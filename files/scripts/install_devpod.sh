c8491c19c572c5b65490900452729d443cc7a37a
anhdong <mobundong@gmail.com>
2026-07-14 15:56:01 +0700

    fix: install devpod in usr/bin  instead of usr/local/bin

--- files/scripts/install_devpod.sh
+++ files/scripts/install_devpod.sh
@@ -6,9 +6,9 @@ echo "Downloading devpod..."
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
 
 echo "Installing..."
-sudo install -c -m 0755 devpod /usr/local/bin 
+sudo install -c -m 0755 devpod /usr/bin 
 
 echo "Cleaning up..."
 rm -f devpod
 
-echo "devpod installed successfully to /usr/local/bin"
+echo "devpod installed successfully to /usr/bin"
